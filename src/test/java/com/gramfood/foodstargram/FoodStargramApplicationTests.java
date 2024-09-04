9827a31d76298ef8ef7a8afe6d6fd99763b4d593
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2024-09-04 21:40:14 +0900

    [박상훈] - Foodstargram 최초 환경설정 완료

---
+++ src/test/java/com/gramfood/foodstargram/FoodStargramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gramfood.foodstargram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodStargramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
