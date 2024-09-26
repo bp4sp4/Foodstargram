c8af6e184ef484cd4d5e9ab23f8fbe8948306fb5
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2024-09-26 21:41:40 +0900

    [박상훈] - 푸터까지 작업완료

--- src/main/java/com/gramfood/foodstargram/config/WebMvcConfig.java
+++ src/main/java/com/gramfood/foodstargram/config/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package com.gramfood.foodstargram.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.gramfood.foodstargram.Interceptor.PermissionInterceptor;
 import com.gramfood.foodstargram.common.FileManager;
 
 @Configuration
@@ -13,5 +15,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**")
 		.addResourceLocations("file:///" + FileManager.FILE_UPLOAD_PATH + "/");
+	}
+	
+	@Override 
+	public void addInterceptors(InterceptorRegistry registry) {
+		PermissionInterceptor intercpetor = new PermissionInterceptor();
+		registry.addInterceptor(intercpetor)
+		.addPathPatterns("/**")
+		.excludePathPatterns("/static/**", "/images/**", "/user/logout");
 	}
 }
